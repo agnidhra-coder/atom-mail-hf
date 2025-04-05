ed2a8ab640767449bbb4344f9534659e6498141d
Agnidhra Sinha <59487124+agnidhra-coder@users.noreply.github.com>
2025-04-05 15:25:59 +0530

    Delete backend/src directory

--- backend/src/routes/emailRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { uploadEmail } from '../controllers/emailController';
-
-const router = express.Router();
-
-router.post('/upload', uploadEmail);
-
-export default router;
